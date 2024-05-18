44b454f0656342bf38fcad77279009b2787d15ee
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-05-18 12:42:03 +0400

    installing dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 
+import { fileURLToPath, URL } from 'node:url'
+
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
@@ -7,8 +9,8 @@ export default defineConfig({
   plugins: [vue()],
   resolve:{
     alias:{
-      src: '/src'
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   }
   
 })
