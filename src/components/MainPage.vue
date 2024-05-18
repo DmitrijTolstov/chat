c964f9d9986b1c02ad0566f192f2970a02cbf4f8
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-05-18 12:46:57 +0400

    main component

---
+++ src/components/MainPage.vue
@@ -0,0 +1,28 @@
+<script setup lang='ts'>
+
+import Chat from '@/components/Chat.vue'
+import Friends from '@/components/Friends.vue'
+import Gallery from '@/components/Gallery.vue'
+import Header from '@/components/Header.vue'
+
+</script>
+<template>
+    <Header></Header>
+    <div class='container'>
+        <Friends/>
+        <Chat/>
+        <Gallery/>
+    </div>
+
+    
+</template>
+<style scoped>
+
+.container{
+    display: flex;
+    justify-content: space-between;
+    gap: 50px;
+    margin: 40px;
+    max-height: 701px;
+}
+</style>
