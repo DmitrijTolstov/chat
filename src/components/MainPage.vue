d913f61afc630765856b82857c76cbdb3534ebb3
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-08-17 01:24:14 +0400

    adding styles in the main page component

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,24 +1,32 @@
 <script setup lang='ts'>
 
-import Chat from '@/components/Chat.vue'
 import Friends from '@/components/Friends.vue'
 import Gallery from '@/components/Gallery.vue'
+import Chat from '@/components/Chat.vue'
 import Header from '@/components/Header.vue'
 
 </script>
 <template>
     <Header></Header>
     <div class='container'>
-        <Friends/>
-        <Chat/>
-        <Gallery/>
+        <Friends />
+        <div class="chat" v-if='false'>
+            <Chat />
+            <Gallery />
+        </div>
+
     </div>
 
-    
+
 </template>
 <style scoped>
+.container {
+    display: flex;
+    margin: 40px;
+    max-height: 701px;
+}
 
-.container{
+.chat {
     display: flex;
     justify-content: space-between;
     gap: 50px;
